6a1f010ebf95ebd409a367d41be4a6a2c1293084
hycst <yang0371@algonquinlive.com>
2026-06-15 15:17:39 -0400

    add variables and resource group

--- main.tf
+++ main.tf
@@ -23,3 +23,27 @@ provider "azurerm" {
 # Cloud-init Provider
 provider "cloudinit" {
 }
+
+
+
+variable "labelPrefix" {
+  description = "yang0371"
+  type        = string
+}
+
+variable "region" {
+  description = "Azure region for all resources."
+  type        = string
+  default     = "eastus"
+}
+
+variable "admin_username" {
+  description = "Admin username for the Linux VM."
+  type        = string
+  default     = "azureadmin"
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "${var.labelPrefix}-A05-RG"
+  location = var.region
+}
