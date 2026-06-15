9089b1a2ca3b27c6910fb952a2af693bff26a93f
hycst <yang0371@algonquinlive.com>
2026-06-15 15:43:48 -0400

    update region to allowed location

--- main.tf
+++ main.tf
@@ -34,7 +34,7 @@ variable "labelPrefix" {
 variable "region" {
   description = "Azure region for all resources."
   type        = string
-  default     = "eastus"
+  default     = "westus2"
 }
 
 variable "admin_username" {
@@ -53,8 +53,11 @@ resource "azurerm_public_ip" "public_ip" {
   name                = "${var.labelPrefix}-A05-public-ip"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  allocation_method   = "Dynamic"
+
+  allocation_method = "Static"
+  sku               = "Standard"
 }
+
 
 resource "azurerm_virtual_network" "vnet" {
   name                = "${var.labelPrefix}-A05-vnet"
