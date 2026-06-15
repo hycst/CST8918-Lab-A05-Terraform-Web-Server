7584b34bf8bac9bb3482fcbe1c071c4c7bf5a893
hycst <yang0371@algonquinlive.com>
2026-06-15 15:20:55 -0400

    add network interface

--- main.tf
+++ main.tf
@@ -102,3 +102,26 @@ resource "azurerm_network_security_group" "nsg" {
     destination_address_prefix = "*"
   }
 }
+
+
+
+resource "azurerm_network_interface" "nic" {
+  name                = "${var.labelPrefix}-A05-nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.public_ip.id
+  }
+}
+
+
+resource "azurerm_network_interface_security_group_association" "nic_nsg" {
+  network_interface_id      = azurerm_network_interface.nic.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
+
+
